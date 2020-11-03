e1a7c5c27dbc06e9109e78cac1d72692a612679e
Grace El Bisimwa <58594917+Jean-pierre14@users.noreply.github.com>
2020-11-03 17:11:28 +0200

    Member index

--- member/index.php
+++ member/index.php
@@ -11,14 +11,14 @@
     <meta charset="UTF-8">
     <title>ERC</title>
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,600|Roboto:400,400i,500" rel="stylesheet">
-    <link rel="stylesheet" href="css/linearicons.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/nice-select.css">
-    <link rel="stylesheet" href="css/hexagons.min.css">
-    <link rel="stylesheet" href="css/owl.carousel.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="../css/linearicons.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/magnific-popup.css">
+    <link rel="stylesheet" href="../css/nice-select.css">
+    <link rel="stylesheet" href="../css/hexagons.min.css">
+    <link rel="stylesheet" href="../css/owl.carousel.css">
+    <link rel="stylesheet" href="../css/main.css">
 </head>
 
 <body>
@@ -33,9 +33,9 @@
                 <nav id="nav-menu-container">
                     <ul class="nav-menu">
                         <li class="menu-active"><a href="index.html">Home</a></li>
-                        <li><a href="#about">About</a></li>
+                        <li><a href="#about">Profile</a></li>
                         <li class="menu-has-children">
-                            <a href="login.php" class="shadow  btn btn-danger">Sign In</a>
+                            <a href="#" class="shadow  btn btn-danger">Logout</a>
                         </li>
                     </ul>
                 </nav>
@@ -410,22 +410,22 @@
 
     <script src="js/vendor/jquery-2.2.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="js/tilt.jquery.min.js"></script>
-    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="../js/tilt.jquery.min.js"></script>
+    <script src="../js/vendor/bootstrap.min.js"></script>
     <script type="text/javascript"
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
-    <script src="js/easing.min.js"></script>
-    <script src="js/hoverIntent.js"></script>
-    <script src="js/superfish.min.js"></script>
-    <script src="js/jquery.ajaxchimp.min.js"></script>
-    <script src="js/jquery.magnific-popup.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/owl-carousel-thumb.min.js"></script>
-    <script src="js/hexagons.min.js"></script>
-    <script src="js/jquery.nice-select.min.js"></script>
-    <script src="js/waypoints.min.js"></script>
-    <script src="js/mail-script.js"></script>
-    <script src="js/main.js"></script>
+    <script src="../js/easing.min.js"></script>
+    <script src="../js/hoverIntent.js"></script>
+    <script src="../js/superfish.min.js"></script>
+    <script src="../js/jquery.ajaxchimp.min.js"></script>
+    <script src="../js/jquery.magnific-popup.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/owl-carousel-thumb.min.js"></script>
+    <script src="../js/hexagons.min.js"></script>
+    <script src="../js/jquery.nice-select.min.js"></script>
+    <script src="../js/waypoints.min.js"></script>
+    <script src="../js/mail-script.js"></script>
+    <script src="../js/main.js"></script>
 
     <script src="../public/js/jquery-Api.js"></script>
 
