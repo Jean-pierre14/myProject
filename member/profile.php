3dfc4a90983c65bc6dc6ef7ae3f529512cf8900f
Grace El Bisimwa <58594917+Jean-pierre14@users.noreply.github.com>
2020-11-04 13:12:09 +0200

    Profile

--- member/profile.php
+++ member/profile.php
@@ -186,133 +186,12 @@
                     <p class="word-day">
                         Word of the day
                     </p>
-                    <a class="primary-btn text-uppercase" href="#">Get Details</a>
+                    <a class="primary-btn shadow text-uppercase" href="profile.php"><i class="fa fa-user"></i> Back</a>
                 </div>
             </div>
         </div>
     </section>
-    <!-- End about Area -->
 
-    <!-- Start Testimonials Area -->
-    <section class="testimonials-area about-page section-gap-bottom">
-        <div class="container">
-            <div class="testi-slider owl-carousel" data-slider-id="1">
-                <div class="item">
-                    <div class="testi-item">
-                        <img src="img/quote.png" alt="">
-                        <h4>Fanny Spencer</h4>
-                        <ul class="list">
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                        </ul>
-                        <div class="wow fadeIn" data-wow-duration="1s">
-                            <p>
-                                As conscious traveling Paup ers we must always be oncerned about our dear Mother Earth.
-                                If you think about it,
-                                you travel
-                                across her face <br> and She is the host to your journey.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="testi-item">
-                        <img src="img/quote.png" alt="">
-                        <h4>Fanny Spencer</h4>
-                        <ul class="list">
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                        </ul>
-                        <div class="wow fadeIn" data-wow-duration="1s">
-                            <p>
-                                As conscious traveling Paup ers we must always be oncerned about our dear Mother Earth.
-                                If you think about it,
-                                you travel
-                                across her face <br> and She is the host to your journey.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="testi-item">
-                        <img src="img/quote.png" alt="">
-                        <h4>Fanny Spencer</h4>
-                        <ul class="list">
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                        </ul>
-                        <div class="wow fadeIn" data-wow-duration="1s">
-                            <p>
-                                As conscious traveling Paup ers we must always be oncerned about our dear Mother Earth.
-                                If you think about it,
-                                you travel
-                                across her face <br> and She is the host to your journey.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="testi-item">
-                        <img src="img/quote.png" alt="">
-                        <h4>Fanny Spencer</h4>
-                        <ul class="list">
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i></a></li>
-                        </ul>
-                        <div class="wow fadeIn" data-wow-duration="1s">
-                            <p>
-                                As conscious traveling Paup ers we must always be oncerned about our dear Mother Earth.
-                                If you think about it,
-                                you travel
-                                across her face <br> and She is the host to your journey.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="owl-thumbs d-flex justify-content-center" data-slider-id="1">
-                <div class="owl-thumb-item">
-                    <div>
-                        <img class="img-fluid" src="img/testimonial/t1.jpg" alt="">
-                    </div>
-                    <div class="overlay overlay-grad"></div>
-                </div>
-                <div class="owl-thumb-item">
-                    <div>
-                        <img class="img-fluid" src="img/testimonial/t2.jpg" alt="">
-                    </div>
-                    <div class="overlay overlay-grad"></div>
-                </div>
-                <div class="owl-thumb-item">
-                    <div>
-                        <img class="img-fluid" src="img/testimonial/t3.jpg" alt="">
-                    </div>
-                    <div class="overlay overlay-grad"></div>
-                </div>
-                <div class="owl-thumb-item">
-                    <div>
-                        <img class="img-fluid" src="img/testimonial/t4.jpg" alt="">
-                    </div>
-                    <div class="overlay overlay-grad"></div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- End Testimonials Area -->
-
-    <!-- Start Footer Area -->
     <footer class="footer-area section-gap">
         <div class="container">
             <div class="row">
@@ -410,7 +289,6 @@
             </div>
         </div>
     </footer>
-    <!-- End Footer Area -->
 
     <script src="js/vendor/jquery-2.2.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
