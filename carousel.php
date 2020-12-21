3de7accc09b6d983ea7bd3ad84535abfe3f25772
Grace El Bisimwa <58594917+Jean-pierre14@users.noreply.github.com>
2020-12-21 19:44:35 +0200

    carousel can be design using foreach loop

--- carousel.php
+++ carousel.php
@@ -5,100 +5,78 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Carousel</title>
-    <!-- <link rel="stylesheet" href="./css/bootstrap.css"> -->
-    <link rel="stylesheet" href="./css/owl.carousel.css">
-    <link rel="stylesheet" href="./css/font-awesome.min.css">
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-    <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-    <script src="./public/js/jquery-3.4.0.min.js"></script>
-    <!-- <script src="./js/owl-carousel-thumb.min.js"></script> -->
-    <script src="./js/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="./public/bootstrap/bootstrap.min.css">
+    <script src="./public/bootstrap/jquery.min.js"></script>
+    <script src="./public/bootstrap/bootstrap.min.js"></script>
+    <script src="./public/bootstrap/popper.min.js"></script>
 </head>
 
 <body>
+
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-                <h3>Carousel reading user imgs</h3>
-                <?php
-                    include("./config/db.php");
-                    $output = '';
-                    $sql = mysqli_query($con, "SELECT * FROM user_account ORDER BY id DESC");
-                    if(@mysqli_num_rows($sql) > 0){
-                        $output .= '
-                        <div id="demo" class="carousel slide" data-ride="carousel">
-                            <div class="carousel-inner">
-                        ';
-                        $active = 1;
-                        while($row = mysqli_fetch_array($sql)){
-                            // $output .= '
-                            // <ul class="carousel-indicators">
-                            //     <li data-target="#demo" data-slide-to="0" class="active"></li>
-                            //     <li data-target="#demo" data-slide-to="1"></li>
-                            //     <li data-target="#demo" data-slide-to="2"></li>
-                            // </ul>
-                            // ';
+        <h2 class="p-2 mt-1 text-center bg-primary text-white">
+            Carousel
+        </h2>
 
-                            if($active == 1):
-                                $output .= '
-                                <div class="carousel-item active">
-                                    <img src="./'.$row['profile_pic'].'" class="img-fluid" alt="'.$row['username'].'">
-                                </div>
-                                ';
-                            else:
-                                $output .= '
-                                <div class="carousel-item">
-                                    <img src="./'.$row['profile_pic'].'" class="img-fluid" alt="'.$row['username'].'">
-                                </div>
-                                ';
-                            endif;
-                        }
-                        $active.= +1;
-                        $output .= '</div>';
-                    }else{
-                        $output .= '<p class="alert alert-warning">there no user registrated</p>';
-                    }
-                ?>
-                <h3>Carousel</h3>
-
-                <?php print $output;?>
-
+        <?php
+        include './config/db.php';
+        $o = '';
+        $select = mysqli_query($con, "SELECT * FROM user_account LIMIT 5");
+        if(mysqli_num_rows($select) > 0):
+            foreach($select as $row):
+                $o .= '
+                <div class="carousel-item">
+                    <img src="./'.$row['profile_pic'].'" alt="Chicago"
+                        width="1100" height="500">
+                    <div class="carousel-caption">
+                        <h3>'.$row['username'].'</h3>
+                        <p>'.$row['about'].'</p>
+                    </div>
+                </div>
+                ';
+            endforeach;
+        else:
+            $o .= '<p class="alert alert-danger">There is no data</p>';
+        endif;
+        ?>
+        <div id="demo" class="carousel slide" data-ride="carousel">
+            <ul class="carousel-indicators">
+                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                <li data-target="#demo" data-slide-to="1"></li>
+                <li data-target="#demo" data-slide-to="2"></li>
+            </ul>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="./assets/images/erc/choir.jpg" alt="Los Angeles" width="1100" height="500">
+                    <div class="carousel-caption">
+                        <h3>Los Angeles</h3>
+                    </div>
+                </div>
+                <?= $o; ?>
+                <div class="carousel-item">
+                    <img src="./assets/images/erc/cyberpunk_egypt_by_kerko_ddxalej-fullview.jpg" alt="Chicago"
+                        width="1100" height="500">
+                    <div class="carousel-caption">
+                        <h3>Chicago</h3>
+                        <p>Thank you, Chicago!</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./assets/images/erc/yvan_profile.jpg" alt="New York" width="1100" height="500">
+                    <div class="carousel-caption">
+                        <h3>New York</h3>
+                        <p>We love the Big Apple!</p>
+                    </div>
+                </div>
             </div>
+            <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </a>
+            <a class="carousel-control-next" href="#demo" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </a>
         </div>
     </div>
-
-
 </body>
-
-<!-- Carousel -->
-<script>
-$('.owl-carous').owlCarousel({
-    loop: true,
-    margin: 90,
-    nav: true,
-    autoplay: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 2
-        },
-        1000: {
-            items: 3
-        }
-    }
-})
-</script>
 
 </html>
--- stackover.php
+++ stackover.php
@@ -1,14 +1,31 @@
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Carousel indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
+<!DOCTYPE html>
+<html lang="en">
 
-    <?php
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./public/css/all.min.css">
+    <link rel="stylesheet" href="./public/bootstrap/bootstrap.min.css">
+    <script src="./public/js/all.min.js"></script>
+    <script src="./public/bootstrap/jquery.min.js"></script>
+    <script src="./public/bootstrap/bootstrap.min.js"></script>
+    <script src="./public/bootstrap/popper.min.js"></script>
+</head>
+
+<body>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Carousel indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <?php
         $dataid;
-                $data = mysqli_query($con,"SELECT * FROM feedback");
+                $con = mysqli_connect("localhost", "root", "", "erc_db") or die("Cannot be connected to this db");
+                $data = mysqli_query($con,"SELECT * FROM user_account limit 5");
                     $count = mysqli_num_rows($data);
                     if ($count != 0) {
                         echo '
@@ -18,7 +35,7 @@
                     <!-- Wrapper for carousel items -->
                     <div class="carousel-inner">        
                         <div class="item carousel-item active">
-                            <p class="testimonial">'. $row['comment'] .'</p>
+                            <p class="testimonial">'. $row['username'] .'</p>
                             <p class="overview"><b>Anonymous</b></p>
                             <div class="star-rating">
                                 <ul class="list-inline">
@@ -36,12 +53,14 @@
             }
           }
         ?>
-    <!-- Carousel controls -->
-    <a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
-        <i class="fa fa-angle-left"></i>
-    </a>
-    <a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
-        <i class="fa fa-angle-right"></i>
-    </a>
-</div>
-</div>
+        <!-- Carousel controls -->
+        <a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
+            <i class="fa fa-angle-left"></i>
+        </a>
+        <a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
+            <i class="fa fa-angle-right"></i>
+        </a>
+    </div>
+</body>
+
+</html>
