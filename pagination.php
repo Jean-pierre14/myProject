56d281568d290895c458090ef46be1641bf2503d
Grace El Bisimwa <58594917+Jean-pierre14@users.noreply.github.com>
2020-12-27 08:18:27 +0200

    Pagination training

---
+++ pagination.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagination</title>
+</head>
+
+<body>
+    <div class="container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>username</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                </tr>
+            </thead>
+            <tbody id="results">
+
+            </tbody>
+        </table>
+    </div>
+    <script src="./public/js/jquery-3.4.0.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            // alert("Good")
+        })
+    </script>
+</body>
+
+</html>
