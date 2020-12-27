a479e65d096e6a2c51080f593898a905467dac34
Grace El Bisimwa <58594917+Jean-pierre14@users.noreply.github.com>
2020-12-27 11:29:28 +0200

    Add pagination buttons

--- config/pagination.php
+++ config/pagination.php
@@ -10,29 +10,32 @@ if(isset($_POST['action'])){
         $totalRows = mysqli_num_rows($AllResult);
         // $pages = $totalRows/6;
 
-        $page = 1;
+        // $page = 1;
+
+        if(isset($_POST['page'])){
+            $page = $_POST['page'];
+        }else{
+            $page = 1;
+        }
+
         $data_per_page = 7;
         $startForm = ($page - 1) * $data_per_page;
         $totalPages = ceil($totalRows / $data_per_page );
 
-        for($i = 1; $i <= $totalPages; $i ++ ){
-            $o .= '
-                <button type="button" class="pageBtn" id="'.$i.'">'.$i.'</button>
-            ';
-        }
 
-
-        $sql = mysqli_query($con, "SELECT * FROM user_account ORDER BY id DESC LIMIT 7");
+        $sql = mysqli_query($con, "SELECT * FROM user_account ORDER BY id DESC LIMIT $startForm,$data_per_page");
 
         if(@mysqli_num_rows($sql) > 0){
             $o .= '
-            <table>
+            <table class="table table-sm table-action table-striped">
                 <thead>
                     <tr>
                         <th>Name</th>
                         <th>username</th>
                         <th>Phone</th>
                         <th>Address</th>
+                        <th>
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
@@ -40,10 +43,16 @@ if(isset($_POST['action'])){
             while($row = mysqli_fetch_array($sql)){
                 $o .= '
                     <tr>
-                        <td>'.$row['name'].'</td>
+                        <td width="30%">'.$row['name'].'</td>
                         <td>'.$row['username'].'</td>
                         <td>'.$row['locatio'].'</td>
                         <td>'.$row['phone'].'</td>
+                        <td>
+                            <div class="btn-group">
+                                <button class="btn btn-sm btn-info">edit</button>
+                                <button class="btn btn-sm btn-danger">delete</button>
+                            </div>
+                        </td>
                     </tr>
                 ';
             }
@@ -54,6 +63,18 @@ if(isset($_POST['action'])){
             // for($i=1;$i<=$pages;$i++){
             //     $o .= '<span> '.$i.' Page</span>';
             // }
+            $o .= '<div class="pagination">
+            ';
+            for($i = 1; $i <= $totalPages; $i ++ ){
+                
+                $o .= '
+                    <button type="button" class="page-link pagination-link shadow-sm pageBtn" id="'.$i.'">'.$i.'</button>
+                ';
+
+            }
+            $o .= '
+            
+            </div>';
             
         }else{
             $o .= '<p>there no data record</p>';
