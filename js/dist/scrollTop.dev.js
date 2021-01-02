0296971ee7ee5b9bc60a5fddc7ccd3b704906d31
Grace El Bisimwa <58594917+Jean-pierre14@users.noreply.github.com>
2021-01-02 11:48:42 +0200

    ScrollTop button to index page

---
+++ js/dist/scrollTop.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+$(document).ready(function () {
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 100) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+  $('#back-to-top').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+});
